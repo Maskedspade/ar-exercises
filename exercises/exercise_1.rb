e0d08ca1dfcd50f5e2b334af7c45694857323d11
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-02-19 22:40:23 +0000

    creates three stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,12 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+def create_store name, a_r, mens, womens
+  Store.create(name: name, annual_revenue: a_r, mens_apparel: mens, womens_apparel: womens)
+end
+
+burnaby = create_store("Burnaby", 300000, true, true)
+richmond = create_store("Richmond", 1260000, false, true)
+gastown = create_store("Gastown", 190000, true, false)
+
+puts "Total number of stores: #{Store.count}"

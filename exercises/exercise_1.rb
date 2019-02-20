610da8aade6e868d4900eddb6ef094dbbe804a00
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-02-20 01:57:09 +0000

    added validations for creating new in table

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,7 +7,7 @@ def create_store name, a_r, mens, womens
   Store.create(name: name, annual_revenue: a_r, mens_apparel: mens, womens_apparel: womens)
 end
 
-burnaby = create_store("Burnaby", 300000, true, true)
+burnaby = create_store("Burnaby")
 richmond = create_store("Richmond", 1260000, false, true)
 gastown = create_store("Gastown", 190000, true, false)
 
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, :annual_revenue, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0}
+  validate :validate_apparel, on: :create
 
+  def validate_apparel
+    (self.errors[:base] << "we need to sell some clothes") unless (mens_apparel || womens_apparel)
+  end
 end
